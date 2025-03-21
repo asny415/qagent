d381fce4144b0d2f84232869ba33f56a531ef974
wesley <9610848@qq.com>
2025-03-21 08:56:39 +0800

    fix iNiverseMixInNode missing

--- src/tools/draw.ts
+++ src/tools/draw.ts
@@ -1,4 +1,4 @@
-import { getEnv } from "../ElectronWindow";
+import { getEnv, iNiverseMixInNode } from "../ElectronWindow";
 export const drawCard_doc: DOC = [
   "抽卡操作使用随机的提示词和随机的大小生成随机的图片",
 ];
@@ -7,5 +7,5 @@ export const drawCard: TOOL_FUNCTION = async () => {
   const width = sizes.sort(() => Math.random() - 0.5)[0];
   const height = sizes.sort(() => Math.random() - 0.5)[0];
   const prompt = await getEnv("DRAW_CARD_PROMPT");
-  return await iNiverseMix({ prompt, width, height });
+  return await iNiverseMixInNode({ prompt, width, height });
 };
