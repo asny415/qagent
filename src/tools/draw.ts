b281e3bf6338d2984fa3d8893e21b0190df7f930
wesley <9610848@qq.com>
2025-03-21 09:01:34 +0800

    doc of draw

--- src/tools/draw.ts
+++ src/tools/draw.ts
@@ -1,6 +1,6 @@
 import { getEnv, iNiverseMixInNode } from "../ElectronWindow";
 export const drawCard_doc: DOC = [
-  "抽卡操作使用随机的提示词和随机的大小生成随机的图片",
+  "抽卡操作，使用随机的提示词和随机的大小生成随机的图片，除非用户明确的用中文提到'抽卡'，否额你不应当调用这个函数",
 ];
 export const drawCard: TOOL_FUNCTION = async () => {
   const sizes = [512, 768, 1024, 1080];
